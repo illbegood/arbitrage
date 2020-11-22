fe3b888027c1e8d2a4f2a1e9349002613c9b4b4c
Сергей Ершов <s.ershov@g.nsu.ru>
2020-11-22 22:38:52 +0700

    negative triangle search code hotfix

--- triangle.py
+++ triangle.py
@@ -7,6 +7,7 @@ def findNegativeTriangles(graph):
 				for w in graph[v]:
 					if w not in visited and isNegative(graph, u, v, w):
 						result.append([u, v, w])
+		visited.add(u)
 
 def isNegative(graph, u, v, w):
 	return graph[u][v] + graph[v][w] + graph[w][u] < 0
