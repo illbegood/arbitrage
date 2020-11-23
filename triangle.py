efb774c2fefaf4737d83232f2eb5d8a859889a80
Сергей Ершов <s.ershov@g.nsu.ru>
2020-11-23 16:17:23 +0700

    negative triangle search small fix

--- triangle.py
+++ triangle.py
@@ -2,12 +2,12 @@ def findNegativeTriangles(graph):
 	result = []
 	visited = set()
 	for u in graph:
+		visited.add(u)
 		for v in graph[u]:
 			if v not in visited:
 				for w in graph[v]:
 					if w not in visited and graph[w][u] in graph[w] and isNegative(graph, u, v, w):
 						result.append([u, v, w])
-		visited.add(u)
 
 def isNegative(graph, u, v, w):
 	return graph[u][v] + graph[v][w] + graph[w][u] < 0
