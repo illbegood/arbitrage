134b01ef0419ea2a90db1e99732c3e21e8567d74
Sergey Ershov <ershov.3r5h0v@gmail.com>
2020-12-04 19:30:07 +0700

    123

--- logger.py
+++ logger.py
@@ -3,13 +3,8 @@ from collections.abc import Iterable
 
 def write(deq, filename='log'):
     logging.basicConfig(filename=filename, level=logging.ERROR, format='%(message)s')
-    message = ' '.join(map(str, deq)) + '\n'
-    logging.error(message)
-    #for e in deq:
-        #if isinstance(e, Iterable):
-            #map(print, deq)
-            #logging.error(str(e))
-            #for i in e:
-                #logging.debug(i)
-        #else:
-            #print(e)
+#    message = ' '.join(map(str, deq)) + '\n'
+#    logging.error(message)
+    for e in deq:
+        message = ' '.join(e) if isinstance(e, Iterable)  else str(e)
+        logging.error(message)
