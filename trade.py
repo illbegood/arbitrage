97a41e89260558805341b5f76e3cdd7b88199860
Alex <alex@MBP-Alex.sib.mts.ru>
2020-12-11 20:44:56 +0700

    added expected profit to trade_args

--- trade.py
+++ trade.py
@@ -14,56 +14,50 @@ def get_trade_args(graph, monograph, cycle, balance):
         logDeque.append('------------------------')
         logDeque.append(cycle)
         max_volume = balance
+        expected_profit = 1
         all_orders = []
         for i in range(len(cycle) - 1):
             x_cur = cycle[i]
             x_next = cycle[i + 1]
             if x_cur in monograph and x_next in monograph[x_cur]:
                 symb = x_cur + x_next
-                #expected_price = math.exp(-graph[x_cur][x_next])
                 order = binance.fetch_orderbook(symb)['asks'][0]
                 price, volume = float(order[0]), float(order[1])
                 all_orders.append([price, volume])
-                #if price > expected_price:
-                #    logDeque.append(format_exc())
-                #    logDeque.append(None)
-                #    return None, None, logDeque
                 if volume * price > max_volume:
                     max_volume /= price
                 else:
                     max_volume = volume
+                expected_profit /= (price * (1 + fee))
                 logDeque.append((x_next + ' volume, ', max_volume))
             else:
                 symb = x_next + x_cur
-                #expected_price = math.exp(-graph[x_next][x_cur])
                 order = binance.fetch_orderbook(symb)['bids'][0]
                 price, volume = float(order[0]), float(order[1])
                 all_orders.append([price, volume])
-                #if price < expected_price:
-                #    logDeque.append(format_exc())
-                #    logDeque.append(None)
-                #    return None, None, logDeque
                 if volume > max_volume:
                     max_volume *= price
                 else:
                     max_volume = volume * price
+                expected_profit *= (price * (1 - fee))
                 logDeque.append((x_next + ' volume, ', max_volume))
-        return max_volume, all_orders, logDeque
+        logDeque.append(('expected_profit: ', expected_profit))
+        return expected_profit, max_volume, all_orders, logDeque
     except:
         if logDeque[-1] is not None:
             logDeque.append(format_exc())
             logDeque.append(None)
-        return None, None, logDeque
+        return None, None, None, logDeque
     
 def trade(graph, monograph, cycle, balance):
     logDeque = deque()
     try:
-        trade_balance, all_orders, innerLogDeque = get_trade_args(graph, monograph, cycle, balance)
+        expected_profit, trade_balance, orders, innerLogDeque = get_trade_args(graph, monograph, cycle, balance)
         logDeque += innerLogDeque
-        if logDeque[-1] is None:
-            raise Exception('')
         if trade_balance < 5:
             raise Exception('low volume: ' + str(trade_balance))
+        if expected_profit <= 1:
+            raise Exception('no profit expected: ' + str(expected_profit))
         logDeque.append(('Total balance:', balance))
         logDeque.append(('Trade balance:', trade_balance))
         for i in range(len(cycle) - 1):
@@ -72,14 +66,14 @@ def trade(graph, monograph, cycle, balance):
             if x_cur in monograph and x_next in monograph[x_cur]:
                 symb = x_cur + '/' + x_next
                 logDeque.append((symb, trade_balance))
-                price = all_orders[i][0]
+                price = orders[i][0]
                 order_volume = truncate(trade_balance / price, precision)
                 logDeque.append(('BUY:', order_volume, price))
                 trade_balance = order_volume * (1 - fee)
             else:
                 symb = x_next + '/' + x_cur
                 logDeque.append((symb, trade_balance))
-                price = all_orders[i][0]
+                price = orders[i][0]
                 order_volume = truncate(trade_balance, precision)
                 logDeque.append(('SELL', order_volume, price))
                 trade_balance = order_volume * price * (1 - fee)
