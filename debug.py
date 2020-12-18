020efd906ad374480687fcccc2def16f7236f755
Sergey Ershov <ershov.3r5h0v@gmail.com>
2020-12-18 23:23:07 +0700

    Removed print_results

--- debug.py
+++ debug.py
@@ -1,38 +1,4 @@
 import math
-from tabulate import tabulate
-
-
-def print_results(graph, path):
-    if path == None:
-        print("No opportunity here :(")
-    else:
-        print(path)
-        graph_sum = 0
-        table = []
-        for i in range(len(path) - 1):
-            weight = graph[path[i]][path[i + 1]]
-            w_e = math.exp(-weight)
-            table.append([w_e, 1/w_e, weight])
-            graph_sum += weight
-        print(tabulate(table, headers=[
-              "CUR1_CUR2", "CUR2_CUR1", "LN(CUR1->CUR2)"]))
-        print('total sum:')
-        print(graph_sum)
-        print('profit')
-        print(math.exp(-graph_sum))
-
-
-def triangles(graph):
-    result = []
-    visited = set()
-    for u in graph:
-        for v in graph[u]:
-            if v not in visited:
-                for w in graph[v]:
-                    if w not in visited and u in graph[w]:
-                        result.append([u, v, w])
-        visited.add(u)
-    return result
 
 from fetch import prefetch
 from trade import trade
